d7f6b6ae60fc168dcce6041ab7da3604b3ea7b84
Bo <arnol229@gmail.com>
2015-12-13 01:07:01 -0600

    better logging

--- drone.py
+++ drone.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
-from __future__ import print_function
 try:
         import spidev
         import time
         import os
         from RPi import GPIO
 except Exception as e:
-    print("messed up: {0}".format(str(e)))
+    print("Error importing module: {0}".format(str(e)))
     exit()
 
 class Drone:
     def __init__(self):
         ## SPI interface for reading the ADC
-        print("Initializing SPI port")
+        print "Initializing SPI port"
         self.__spi=spidev.SpiDev()
         self.__spi.open(0,0)
         ## ADC channel on the ADC chip
@@ -32,7 +31,7 @@ class Drone:
         self.__led_g=None
 
         ## Initializing motor
-        print("Initializing motor")
+        print "Initializing motor"
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.__motor_pwm_pin, GPIO.OUT)
         self.pwm=GPIO.PWM(self.__motor_pwm_pin,self.__starting_freq)
@@ -47,24 +46,16 @@ class Drone:
         try:
             while True:
                 val = ((self.read_adc(self.__joy_x_channel)/10)-100)*-1
-                print("--------------------------------------------\n"+"Value: {0}".format(val), end='\r')
+                print "Value: {0}\r".format(val)
         except KeyboardInterrupt:
-            print("Exiting flying mode")
+            print "Exiting flying mode")
         except Exception as e:
-            print("error while flying: {0}".format(str(e)))
+            print "error while flying: {0}".format(str(e))
 
-    # def read_adc(adcnum):
-    #     if((adcnum > 7) or (adcnum < 0)):
-    #         return -1
-    #     r = self.__spi.xfer2([1,(8+adcnum)<<4,0])
-    #     adcout = ((r[1]&3 << 8) + r[2])
-    #     return adcout
-
-
-print("well its something")
 if __name__ == "__main__":
-    print ("Drone initializing...")
+    print "Drone initializing..."
     drone = Drone()
     time.sleep(2)
-    print ("starting to fly")
+    print "starting to fly"
     drone.fly()
+    GPIO.cleanup()
